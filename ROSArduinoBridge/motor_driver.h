429c836664982ddd3d8b9f600cadebd177b85fbf
AlvaroBarry <abarrigonfer@gmail.com>
2023-04-24 11:42:21 +0200

    real robot working

--- ROSArduinoBridge/motor_driver.h
+++ ROSArduinoBridge/motor_driver.h
@@ -3,12 +3,12 @@
    *************************************************************/
 
 #ifdef L298_MOTOR_DRIVER
-  #define RIGHT_MOTOR_BACKWARD 7 //8 o 7
-  #define LEFT_MOTOR_BACKWARD  6 //4 o 6
-  #define RIGHT_MOTOR_FORWARD  8 //8 o 7
-  #define LEFT_MOTOR_FORWARD   4 //4 o 6
-  #define RIGHT_MOTOR_ENABLE 9 //9
-  #define LEFT_MOTOR_ENABLE 5 //5
+  #define RIGHT_MOTOR_BACKWARD 9 //10 o 9
+  #define LEFT_MOTOR_BACKWARD  6 //5 o 6
+  #define RIGHT_MOTOR_FORWARD  10 //10 o 9
+  #define LEFT_MOTOR_FORWARD   5 //5 o 6
+  #define RIGHT_MOTOR_ENABLE 8 //8
+  #define LEFT_MOTOR_ENABLE 7 //7-´´
 #endif
 
 void initMotorController();
